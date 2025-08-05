410e937f78eb5e0a69aaab4b3ba0e9b1d366d37b
Rukas <45427438+Rukas90@users.noreply.github.com>
2025-08-05 21:09:41 +0300

    Added an Additional Scroll Event Listener for Height Adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,15 @@ function App() {
       const vh = window.innerHeight * 0.01
       document.documentElement.style.setProperty("--vh", `${vh}px`)
     }
-
     setVh()
     window.addEventListener("resize", setVh)
-    return () => window.removeEventListener("resize", setVh)
-  }, [])
+    window.addEventListener("scroll", setVh)
 
+    return () => {
+      window.removeEventListener("resize", setVh)
+      window.removeEventListener("scroll", setVh)
+    }
+  }, [])
   return (
     <ThemeProvider>
       <ForegroundProvider>
